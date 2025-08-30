ed52b532c12addc7743760e85334fd72bb245a4d
O V V S Ganapathi <othojuganapathi123@gmail.com>
2025-08-30 17:31:01 +0530

    added alt attribute for image in topbar

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -8,7 +8,7 @@ export const TopBar = () => {
             <div className="flex gap-5 items-center ml-auto">
                 <Settings className="w-5 h-5 text-gray-600 hover:text-gray-900 cursor-pointer" />
                 <Avatar>
-                    <AvatarImage src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&cs=tinysrgb&w=100&h=100&fit=crop&crop=face" />
+                    <AvatarImage alt="User profile image" src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&cs=tinysrgb&w=100&h=100&fit=crop&crop=face" />
                     <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
             </div>
